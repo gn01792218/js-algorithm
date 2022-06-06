9e9f03d52ed398f9bed28ae8a94656c0b8c87f51
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-06-06 17:36:31 +0800

    增加氣泡排序法

---
+++ bubbleSort.js
@@ -0,0 +1,17 @@
+function swap(arr,index1,index2){
+    [arr[index1],arr[index2]] = [arr[index2],arr[index1]]
+}
+function bubbleSort(arr){
+    for(let i =0 ; i <arr.length-1;i++){
+        let hasSwap = false
+        for(let j = 0 ; j<arr.length-1-i; j++){
+            //如果每一個和下一個相比比較大，就交換
+            if(arr[j]>arr[j+1]) {
+                hasSwap = true
+                swap(arr,j,j+1)
+            }
+        }
+        if(!hasSwap) break  //假如前一次循環沒有出現過交換，表示已經排序完成，不必再排了
+    }
+    return arr
+}
